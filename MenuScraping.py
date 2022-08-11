cfbdb6061be43e9a8adbb4ad9bb17f7217e62f86
Ryosuke Ito <suzusuke0806@gmail.com>
2022-08-11 21:56:35 +0900

    add service on menuScraping

--- MenuScraping.py
+++ MenuScraping.py
@@ -10,53 +10,94 @@ class Menu:
         self.Ingredient = ingredient
 
 
-def getMenu(page, menuId):
-    page.goto(f"https://park.ajinomoto.co.jp/recipe/card/{menuId}/")
+def getMenu(page, menuIdList):
+    menu = []
 
-    recipeCardXpath = (
-        "xpath="
-        "html/body/"
-        "div[@class='chFixed']/"
-        "div[@class='searchView']/"
-        "div[@id='content']/"
-        "article[@id='recipeCard']/"
-    )
+    for index, menuId in enumerate(menuIdList):
+        page.goto(f"https://park.ajinomoto.co.jp/recipe/card/{menuId}/")
 
-    menuTitleLocator = page.locator(
-        recipeCardXpath +
-        "div[@class='recipeArea']/"
-        "div[@class='recipeTitleAreaType02']/"
-        "div[@class='wrap1160']/"
-        "div[@class='in_table']/"
-        "h1[@class='recipeTitle']/"
-        "span"
-    )
-    menuTitle = menuTitleLocator.all_inner_texts()
-
-    menuIngredientItemLocator = page.locator(
-        recipeCardXpath +
-        "div[@class ='recipeCardSpOrderWrap']/"
-        "div[@class ='wrap820 recipeCardSpOrder1']/"
-        "div[@class ='recipeMaterialType02']/"
-        "div[@class ='recipeMaterialList']/"
-        "dl/dt"
-    )
-    menuIngredientItem = menuIngredientItemLocator.all_inner_texts()
-
-    menuIngredientQuantityLocator = page.locator(
-        recipeCardXpath +
-        "div[@class ='recipeCardSpOrderWrap']/"
-        "div[@class ='wrap820 recipeCardSpOrder1']/"
-        "div[@class ='recipeMaterialType02']/"
-        "div[@class ='recipeMaterialList']/"
-        "dl/dd"
-    )
-    menuIngredientQuantity = menuIngredientQuantityLocator.all_inner_texts()
-
-    return Menu(
-        menuTitle[len(menuTitle) - 1],
-        Ingredient(
-            menuIngredientItem,
-            menuIngredientQuantity,
+        recipeCardXpath = (
+            "xpath="
+            "html/body/"
+            "div[@class='chFixed']/"
+            "div[@class='searchView']/"
+            "div[@id='content']/"
+            "article[@id='recipeCard']/"
         )
+
+        menuTitleLocator = page.locator(
+            recipeCardXpath +
+            "div[@class='recipeArea']/"
+            "div[@class='recipeTitleAreaType02']/"
+            "div[@class='wrap1160']/"
+            "div[@class='in_table']/"
+            "h1[@class='recipeTitle']/"
+            "span"
+        )
+        menuTitle = menuTitleLocator.all_inner_texts()
+
+        menuIngredientItemLocator = page.locator(
+            recipeCardXpath +
+            "div[@class ='recipeCardSpOrderWrap']/"
+            "div[@class ='wrap820 recipeCardSpOrder1']/"
+            "div[@class ='recipeMaterialType02']/"
+            "div[@class ='recipeMaterialList']/"
+            "dl/dt"
+        )
+        menuIngredientItem = menuIngredientItemLocator.all_inner_texts()
+
+        menuIngredientQuantityLocator = page.locator(
+            recipeCardXpath +
+            "div[@class ='recipeCardSpOrderWrap']/"
+            "div[@class ='wrap820 recipeCardSpOrder1']/"
+            "div[@class ='recipeMaterialType02']/"
+            "div[@class ='recipeMaterialList']/"
+            "dl/dd"
+        )
+        menuIngredientQuantity = menuIngredientQuantityLocator.all_inner_texts()
+
+        menu.append(
+            Menu(
+                menuTitle[len(menuTitle) - 1],
+                Ingredient(
+                    menuIngredientItem,
+                    menuIngredientQuantity,
+                )
+            )
+        )
+
+    return menu
+
+
+class MenuStruct:
+    def __init__(self, menuBase, menuIngredient):
+        self.menuBase = menuBase
+        self.menuIngredient = menuIngredient
+
+
+def convertMenu(menuList):
+    menuBase = []
+    menuIngredient = []
+    ingredientMasterIndex = 0
+
+    for menuIndex, menu in enumerate(menuList):
+        menuBase.append(
+            [
+                menuIndex+1,
+                menu.title,
+            ]
+        )
+        for ingredientIndex, ingredient in enumerate(menu.Ingredient.item):
+            menuIngredient.append(
+                [
+                    ingredientMasterIndex+1,
+                    menu.Ingredient.item[ingredientIndex],
+                    menu.Ingredient.quantity[ingredientIndex],
+                ]
+            )
+            ingredientMasterIndex = ingredientMasterIndex+1
+
+    return MenuStruct(
+        menuBase,
+        menuIngredient,
     )
--- test_MenuScraping.py
+++ test_MenuScraping.py
@@ -1,17 +1,56 @@
 from playwright.sync_api import Page
 
-from MenuScraping import getMenu
+from MenuScraping import getMenu, Menu, Ingredient, convertMenu
 
 
-def test_menuScraping(page: Page):
+def test_menuScrapingController(page: Page):
     menuIdList = [705651, 708962]
 
     expectedMenuTitle = ['鶏のから揚げ', '鶏胸肉のうま酢煮']
     expectedMenuIngredientItem = ['鶏もも肉・または鶏ももから揚げ用肉', '鶏むね肉']
     expectedMenuIngredientQuantity = ['1枚（250g）', '300g']
 
+    menu = getMenu(page, menuIdList)
+
     for index, menuId in enumerate(menuIdList):
-        menu = getMenu(page, menuId)
-        assert menu.title == expectedMenuTitle[index]
-        assert menu.Ingredient.item[0] == expectedMenuIngredientItem[index]
-        assert menu.Ingredient.quantity[0] == expectedMenuIngredientQuantity[index]
+        assert menu[index].title == expectedMenuTitle[index]
+        assert menu[index].Ingredient.item[0] == expectedMenuIngredientItem[index]
+        assert menu[index].Ingredient.quantity[0] == expectedMenuIngredientQuantity[index]
+
+
+def test_menuScrapingService():
+    # TODO: DIした実装になるようにリファクタリングのこと
+    stubMenuList = [
+        Menu(
+            'menuTitleOne',
+            Ingredient(
+                ['itemOneOne', 'itemOneTwo'],
+                ['11', '12']
+            )
+        ),
+        Menu(
+            'menuTitleTwo',
+            Ingredient(
+                ['itemTwoOne', 'itemTwoTwo', 'itemTwoThree'],
+                ['21', '22', '23']
+            )
+        )
+    ]
+
+    actualMenuOutput = convertMenu(stubMenuList)
+
+    expectedMenuBase = [
+        [1, 'menuTitleOne'],
+        [2, 'menuTitleTwo'],
+    ]
+
+    expectedMenuIngredient = [
+        [1, 'itemOneOne', '11'],
+        [2, 'itemOneTwo', '12'],
+        [3, 'itemTwoOne', '21'],
+        [4, 'itemTwoTwo', '22'],
+        [5, 'itemTwoThree', '23'],
+    ]
+
+    assert actualMenuOutput.menuBase == expectedMenuBase
+    assert actualMenuOutput.menuIngredient == expectedMenuIngredient
